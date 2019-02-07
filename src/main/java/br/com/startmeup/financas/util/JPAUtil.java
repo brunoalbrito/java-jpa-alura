624746657d15917022bea5d841fb6e7b7641737a
unknown <brunoalbuquerquebrito@outlook.com>
2019-02-07 19:59:33 -0200

    Adicionando lombok e tentando fazer jsf funcionar

--- src/main/java/br/com/startmeup/financas/util/JPAUtil.java
+++ src/main/java/br/com/startmeup/financas/util/JPAUtil.java
@@ -6,7 +6,7 @@ import javax.persistence.Persistence;
 
 public class JPAUtil {
 
-    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("startmeup");;
+    private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("postgres");;
 
     public static EntityManager getEntityManager(){
         return emf.createEntityManager();
